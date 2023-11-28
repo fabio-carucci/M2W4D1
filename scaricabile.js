aae535d097cda318a08ff3c4019c2bbda22f37eb
Fabio Carucci <garu2000@live.it>
2023-11-29 00:21:34 +0100

    Part 1 completed

--- scaricabile.js
+++ scaricabile.js
@@ -1,41 +1,43 @@
-/*
-PARTE 1: 
-Oggi analizzeremo un problema molto comune: realizzare algoritmi di ricerca.
-Il tuo compito è creare una funzione che cercherà per posizione lavorativa E posizione geografica. Questi due valori verranno passati come parametri
-Ti abbiamo fornito un array chiamato "jobs" in fondo al file, NON modificarlo in alcun modo.
-L'algoritmo che devi realizzare cercherà SIA per posizione lavorativa che per posizione geografica.
-Prendi queste tre inserzioni ad esempio:
+// PARTE 1: 
+// Oggi analizzeremo un problema molto comune: realizzare algoritmi di ricerca.
+// Il tuo compito è creare una funzione che cercherà per posizione lavorativa e posizione geografica. 
+// Questi due valori verranno passati come parametri
+// Ti abbiamo fornito un array chiamato "jobs" in fondo al file, NON modificarlo in alcun modo.
+// L'algoritmo che devi realizzare cercherà SIA per posizione lavorativa che per posizione geografica.
+// Prendi queste tre inserzioni ad esempio:
 
-      job1:  location: "NY, US",     title: "java dev"
-      job2:  location: "Genoa, IT"   title: "web dev"
-      job3:  location: "US"      title: "dev"
+      // job1:  location: "NY, US",     title: "java dev"
+      // job2:  location: "Genoa, IT"   title: "web dev"
+      // job3:  location: "US"      title: "dev"
 
-Cercando contemporaneamente come posizione lavorativa "dev" e posizione geografica "US", dovresti ottenere come risultato solamente job1 e job3,
-in quanto job2 non soddisfa la condizione posta sulla posizione geografica.
+// Cercando contemporaneamente come posizione lavorativa "dev" e posizione geografica "US", dovresti ottenere come risultato 
+// solamente job1 e job3, in quanto job2 non soddisfa la condizione posta sulla posizione geografica.
 
-REQUISITI:
-- il tuo algoritmo deve tornare i risultati nella seguente forma:
-{
-  result: [], <-- inserisci qui le inserzioni che rispecchiano la posizione lavorativa e la posizione geografica richiesta
-  count: 0 <-- inserisci qui il numero totale delle inserzioni trovate
-}
+// REQUISITI:
+// - il tuo algoritmo deve tornare i risultati nella seguente forma:
+// {
+//   result: [], <-- inserisci qui le inserzioni che rispecchiano la posizione lavorativa e la posizione geografica richiesta
+//   count: 0 <-- inserisci qui il numero totale delle inserzioni trovate
+// }
 
-- da ogni inserzione trovata, elimina i campi "description", "requirements", "benefits" e "company_profile" per semplificare il risultato
+// - da ogni inserzione trovata, elimina i campi "description", "requirements", "benefits" e "company_profile" per semplificare 
+// il risultato
 
-- la tua ricerca deve essere "case insensitive" (non deve essere influenzata da lettere maiuscole o minuscole nelle parole cercate). Questo e' possibile trasformando tutto in lettere minuscole con .toLowerCase()
+// - la tua ricerca deve essere "case insensitive" (non deve essere influenzata da lettere maiuscole o minuscole
+//   nelle parole cercate). Questo e' possibile trasformando tutto in lettere minuscole con .toLowerCase()
 
 
-PARTE 2: 
-Nella pagina HTML, inserisci 2 input di tipo testo (uno per la location e uno per il titolo lavorativo, ricordati di diversificarli con un id) e un bottone con valore “cerca”
+// PARTE 2: 
+// Nella pagina HTML, inserisci 2 input di tipo testo (uno per la location e uno per il titolo lavorativo, ricordati di 
+// diversificarli con un id) e un bottone con valore “cerca”
 
-Al click del bottone, il codice deve raccogliere i valori dei due input e darli in pasto alla funzione che hai creato nella parte 1. 
+// Al click del bottone, il codice deve raccogliere i valori dei due input e darli in pasto alla funzione
+// che hai creato nella parte 1. 
 
-Dopo aver raccolto ed elaborato i dati, e’ il momento di mostrare i risultati sulla pagina: 
-    Puoi scegliere tu se utilizzare un semplice ul / li oppure una tabella 
-    Vai passo per passo e usa molti console.log per capire eventualmente dove sbagli
-    SUGGERIMENTO: ti servira’ un ciclo for!
-
-*/
+// Dopo aver raccolto ed elaborato i dati, e’ il momento di mostrare i risultati sulla pagina: 
+//     Puoi scegliere tu se utilizzare un semplice ul / li oppure una tabella 
+//     Vai passo per passo e usa molti console.log per capire eventualmente dove sbagli
+//     SUGGERIMENTO: ti servira’ un ciclo for!
 
 // NON MODIFICARE QUESTO ARRAY!
 const jobs = [
@@ -125,3 +127,28 @@ const jobs = [
   },
 ]
 
+// Declaration and empty assignment of the final object where the results will be inserted
+
+const finalObject = {
+  result: [], // insertions that match the required job position and geographic location.
+  count: 0, // number of insertions found
+}
+
+// Creation of a function that searches by title and location in the array of objects "jobs" 
+// and inserts the result into the final object.
+// Use of '.toLowerCase' to make the search case-insensitive.
+
+function SearchingAlgoritm (title, location) {
+  for (let i = 0; i < jobs.length; i++) {
+    let JobsElement = jobs[i];
+    if ((JobsElement.title.toLowerCase().includes(title.toLowerCase())) 
+    && (JobsElement.location.toLowerCase().includes(location.toLowerCase()))) {
+      finalObject.result.push(JobsElement);
+      finalObject.count += 1;
+    }
+  }
+}
+
+// Console.log for checking to understand if it works correctly."
+SearchingAlgoritm("Service", "us");
+console.log(finalObject);
